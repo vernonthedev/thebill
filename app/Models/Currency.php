74b2b6453a3afa3a87396747fa8118a360b87e07
vernonthedev <techjaja2@gmail.com>
2025-03-29 19:22:04 +0300

    #update: customizing single funcs for the api for getting collections only

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use ApiPlatform\Metadata\ApiResource;
+// use ApiPlatform\Metadata\Get;
+// use ApiPlatform\Metadata\GetCollection;
 
 // Expose the currency model as an api to api platform
 #[ApiResource(
     paginationItemsPerPage:1,
+    // operations:[
+    //     new GetCollection(),
+    //     new Get(),
+    // ]
 )]
 class Currency extends Model
 {
