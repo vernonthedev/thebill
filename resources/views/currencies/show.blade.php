2bf11f143e9e63e3e3c9a638e7a7a2897c62cb7c
vernonthedev <techjaja2@gmail.com>
2025-03-29 19:01:27 +0300

    #update: added base app ui layout to currency view

--- resources/views/currencies/show.blade.php
+++ resources/views/currencies/show.blade.php
@@ -1,7 +1,9 @@
-<div class="container">
-    <h2>currency Details</h2>
-     <p><strong>currency_name:</strong> {{ $currency ->currency_name }}</p>
-<p><strong>market_cap:</strong> {{ $currency ->market_cap }}</p>
-<p><strong>duration:</strong> {{ $currency ->duration }}</p>
+<x-app-layout>
+    <div class="container">
+        <h2>currency Details</h2>
+        <p><strong>currency_name:</strong> {{ $currency->currency_name }}</p>
+        <p><strong>market_cap:</strong> {{ $currency->market_cap }}</p>
+        <p><strong>duration:</strong> {{ $currency->duration }}</p>
 
-</div>
+    </div>
+</x-app-layout>
