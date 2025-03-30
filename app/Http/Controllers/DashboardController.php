2bdb0ba38d0841bc242e934d8c998e894bca522b
vernonthedev <techjaja2@gmail.com>
2025-03-30 14:19:04 +0300

    #config: setup dummy chart and yet to be rendered in temp

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Charts\UserChart;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard');
+        // create a temp chart with temp data
+        $chart = new UserChart();
+        $chart->labels(['One','Two','Three','Four']);
+        $chart->dataset('My dataset','line',[1,2,3,4]);
+        $chart->dataset('My dataset 2','line',[4,3,2,1]);
+
+        // send the chart info to the temp
+        return view('dashboard', compact('chart'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+return view('welcome');
 });
 
 
